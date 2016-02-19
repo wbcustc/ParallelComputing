334c7a875570089703c28dee6728d99ee297b654
wbcustc <victorwang.bc@gmail.com>
2016-02-19 15:19:49 -0500

    lab1

---
+++ lab1/bandwidth_latency.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#define BLOCK_SIZE 1024
+#define REP_TIMES 1000
+
+int main(int argc, char **argv){
+  int sz,myid;
+  double starttime,endtime;
+  MPI_Init(&argc,&argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &sz);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+  int data[BLOCK_SIZE];
+  for(int i = 0;i < BLOCK_SIZE;i++){
+    data[i] = i;
+  }
+  int dest = sz - myid - 1; 
+  MPI_Barrier(MPI_COMM_WORLD);
+  // Change the data block size to show the impact of the bandwidth.
+  for(int block = 1;block <= BLOCK_SIZE;block *= 2){ 
+    
+    if (myid < sz/2) {
+      
+      starttime = MPI_Wtime();
+      // Send 1000 times to calculate the average message latency. 
+      for(int i = 0;i<REP_TIMES;i++){
+        MPI_Send(data, block, MPI_INT, dest, 0,
+                   MPI_COMM_WORLD);
+      
+        MPI_Recv(data, block, MPI_INT, dest, 0,
+                  MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      }
+      endtime = MPI_Wtime();
+      printf("That took %f milliseconds\n",(endtime-starttime)*1000000.0/REP_TIMES);
+    } else {
+      for(int i = 0;i<REP_TIMES;i++){
+      MPI_Recv(data, block, MPI_INT, dest, 0,
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      
+      MPI_Send(data, block, MPI_INT, dest, 0,
+                MPI_COMM_WORLD);
+      }
+    }
+  } 
+  MPI_Finalize ();
+  exit (0);
+}
---
+++ lab1/dot_product.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#define VECTOR_SIZE 1024*1024
+
+void init_vec(float *vec, int n){
+	srand(time(NULL));
+	for(int i = 0;i < n;i++){
+		vec[i] = rand()/1000000000.0;
+	}
+}
+
+float calculate(float *vec1,float *vec2, int n){
+	float sum = 0.0;
+	for(int i = 0;i<n;i++){
+		sum += (*(vec1+i)) * (*(vec2+i));
+	}
+	return sum;
+}
+
+int main(int argc, char **argv){
+	int sz,myid;
+	double starttime,endtime;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &sz);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+	float *vec1,*vec2;
+	int blocksize = VECTOR_SIZE/sz;
+	float local,global;
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	if(myid == 0){
+		vec1 = (float *)malloc(VECTOR_SIZE*sizeof(float));
+		vec2 = (float *)malloc(VECTOR_SIZE*sizeof(float));
+		init_vec(vec1, VECTOR_SIZE);
+		init_vec(vec2, VECTOR_SIZE);
+		starttime = MPI_Wtime();
+
+	}
+
+    float *temp_vec1 = (float *)malloc(blocksize*sizeof(float));
+	float *temp_vec2 = (float *)malloc(blocksize*sizeof(float));
+	
+	// Dispatch the data chunk into different processors from processor 0.
+	MPI_Scatter(vec1, blocksize, MPI_FLOAT, temp_vec1,
+            blocksize, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+	MPI_Scatter(vec2, blocksize, MPI_FLOAT, temp_vec2,
+            blocksize, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+	local = calculate(temp_vec1,temp_vec2,blocksize);
+
+	// Reduce data from other processors to processor 0.
+	MPI_Reduce(&local, &global, 1, MPI_FLOAT, MPI_SUM, 0, 
+		MPI_COMM_WORLD);
+	
+	if(myid == 0) {
+		endtime = MPI_Wtime();
+		printf("Time cost is %lf\n", (endtime-starttime)*1000000.0);
+		printf("Product is %lf\n", global);
+	}
+
+	MPI_Finalize();
+	exit (0);
+}
